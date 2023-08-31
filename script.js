684cc128a10c334a5a1a0ef4125ffe6ef96e5ea2
Lenzlee Wheeler <lwheeler@luc.edu>
2023-08-31 00:01:18 -0500

    first set of edits

--- script.js
+++ script.js
@@ -3,23 +3,65 @@ $(document).ready(function () {
   // display current date
   $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
 
+  // set elements - API Key
+  // API Key for all weather data 
+var APIKey = "e7870e69c64fe6687d1c5339370055dd";
+var city = "";
+
   // function for when the Search Button is clicked
-  $('.searchBtn').on('click', function () {
+  $('.searchBtn').on('click', function (event) {
 
-    //get user input for City and save in localStorage
-    // var userInput = $(this).siblings('textarea').val();
-    // var currentTime = $(this).parent().attr('id');
+    event.preventDefault();
 
-    // localStorage.setItem(currentTime, userInput);
-  });
+    //get user input for City then save info to localStorage
 
-  // load data from localStorage (city, date, temp, wind, humidity)
-  // $('#hour-9 .description').val(localStorage.getItem('xxx'));
+    city = $("#formInputCity").val();
+    if (city === '') {
+        return alert('Enter City');
+    }
+    getWeatherInfo(city);
 
-  // function to pull 5-Day forecast
-  $(function weekForecast() {
-
+    saveInfoToLocalStorage(city);
 
   });
+
+  //function for getting weather info
+  function getWeatherInfo(city) {
+    var queryAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+    $.ajax({
+        // gets the current weather info
+        url: queryAPI,
+        method: "GET",
+        error: (err => { //If API through error then alert 
+            alert("City not found")
+            return;
+          })
+    }).then(function (response) {
+        console.log(response)
+    });
+}
+
+// function for saving info to localstorage
+function saveInfoToLocalStorage(city) {
+  var data = localStorage.getItem('cities-recently-searched');
+  if (data) {
+      console.log(data, city)
+
+  } else {
+      data = city;
+      localStorage.setItem('cities-recently-searched', data);
+  }
+  if (data.indexOf(city) === -1) {
+      data = data + ',' + city;
+      localStorage.setItem('cities-recently-searched', data);
+  }
+}
+
+  // 5 Day forecast display function
+
+  // Get data store from local storage function
+
+  // Recent Cities List function
+
 
 });
